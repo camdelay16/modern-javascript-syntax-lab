5ee1f9cba5b291349b93feb4c866c85f5d0d16b3
camdelay16 <camdelay16@gmail.com>
2024-10-28 10:15:59 -0400

    completed exercise 10

--- exercises.js
+++ exercises.js
@@ -118,14 +118,28 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // "LANG is equal to localLangConfig or the default value of English."
 
-const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+// const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
 
-// a. Create a variable called LANG
-// b. Assign LANG the value of localLangConfig or 'en' as a default
+// // a. Create a variable called LANG
+// // b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// // Your code here
+// const LANG = localLangConfig || "En"
+
+
+// // Log the result
+// console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
 
 // Your code here
-const LANG = localLangConfig || "En"
-
-
+const USER_THEME = userSavedTheme || 'light'
 // Log the result
-console.log('Language setting:', LANG);
+console.log('User theme setting:', USER_THEME);
+
+
