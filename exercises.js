653fda98bacf29a9390427e1df22b0dbd670266a
camdelay16 <camdelay16@gmail.com>
2024-10-28 09:30:15 -0400

    exercise three complete

--- exercises.js
+++ exercises.js
@@ -14,9 +14,22 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 // Exercise 2: Array destructuring
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
 
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
-// Your code here
-const [firstTopping, secondTopping] = pizzaToppings
-console.log(firstTopping)
-console.log(secondTopping)
+// // Your code here
+// const [firstTopping, secondTopping] = pizzaToppings
+// console.log(firstTopping)
+// console.log(secondTopping)
+
+// Exercise 3: Destructuring objects
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+//   };
+  
+//   // Your code here
+// const { make, model } = car
+// console.log(make)
+// console.log(model)  
