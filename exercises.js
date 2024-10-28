3dd64dff9ec44797e7004e709e96c2f6ade6a1e3
camdelay16 <camdelay16@gmail.com>
2024-10-28 10:20:15 -0400

    completed exercise 11

--- exercises.js
+++ exercises.js
@@ -132,14 +132,25 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // 2. SET WEBSITE THEME
 
-const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+// const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
 
-// a. Create a variable called USER_THEME
-// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+// // a. Create a variable called USER_THEME
+// // b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
 
-// Your code here
-const USER_THEME = userSavedTheme || 'light'
-// Log the result
-console.log('User theme setting:', USER_THEME);
+// // Your code here
+// const USER_THEME = userSavedTheme || 'light'
+// // Log the result
+// console.log('User theme setting:', USER_THEME);
 
 
+// Exercise 11: Optional chaining
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+    name: 'Alice',
+  };
+  
+  let cat = adventurer.cat?.age; // Your code here
+  
+  console.log(cat);
+  
