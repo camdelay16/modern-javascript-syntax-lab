5c185d16a259436f07c2458517160e13ef376fc7
camdelay16 <camdelay16@gmail.com>
2024-10-28 09:34:00 -0400

    completed exercise 4

--- exercises.js
+++ exercises.js
@@ -33,3 +33,12 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 // const { make, model } = car
 // console.log(make)
 // console.log(model)  
+
+// Exercise 4: Applying the spread operator on arrays
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const controversialPizzaToppings = [...pizzaToppings]
+console.log(controversialPizzaToppings)
